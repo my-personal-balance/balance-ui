510a38dd20205e7086d55dfc53e46b383872d780
Juliano Vidal <juliano.vidal@glovoapp.com>
2021-08-04 12:35:19 +0200

    Table description filter

--- src/components/Transactions/TransactionTable.js
+++ src/components/Transactions/TransactionTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {
   Button,
   Col,
+  Input,
   Popconfirm,
   Row,
   Space,
@@ -12,6 +13,7 @@ import {
 import {
   DeleteOutlined,
   EditOutlined,
+  SearchOutlined,
 } from '@ant-design/icons';
 
 import AddTransaction from './AddTransaction';
@@ -78,6 +80,61 @@ const TransactionTable = (props) => {
       }
     });
   };
+
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) => record[dataIndex] ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()) : '',
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = clearFilters => {
+    clearFilters();
+    setSearchText("");
+  };
   
   
   const columns = [
@@ -102,14 +159,19 @@ const TransactionTable = (props) => {
         );
       }
     },
-    { title: 'Description', dataIndex: 'description', key: 'description', },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+      ...getColumnSearchProps('description'),
+    },
     { 
       title: "Category",
       dataIndex: "tag",
       key: "tag", 
       render: (tag, record) => (
         <Tag key={record.id}>{tag ? tag.value : '' }</Tag>
-      )
+      ),
     },
     {
       title: "Account",
