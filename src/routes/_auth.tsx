f1c30264912e1e9aa2d159eafdaafbb5e514788f
Juliano Vidal <juliano.vidal@pandadoc.com>
2025-06-12 19:19:25 +0200

    fix: clean imports

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -1,11 +1,7 @@
-import { BreadcrumbItem, BreadcrumbPage } from '@/components/ui/breadcrumb'
-import { BreadcrumbList } from '@/components/ui/breadcrumb'
-import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
+import { SidebarInset } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
-import { Separator } from '@/components/ui/separator'
-import { Breadcrumb } from '@/components/ui/breadcrumb'
 
 export const Route = createFileRoute('/_auth')({
   beforeLoad: ({ context, location }: { context: any, location: any }) => {
