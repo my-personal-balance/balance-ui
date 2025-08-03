968c1f5ea25ff3df014b482ca6382ea2da6e7dd1
Juliano Vidal <juliano.vidal@pandadoc.com>
2025-08-03 19:25:40 +0200

    fix: revert zod upgrade

--- src/app/settings/profile-form.tsx
+++ src/app/settings/profile-form.tsx
@@ -26,7 +26,7 @@ const profileFormSchema = z.object({
     }),
   email: z
     .string({
-      error: 'Please select an email to display.',
+      required_error: 'Please select an email to display.',
     })
     .email(),
 })
